3689be722df6aacc3e6b3e18b50d4b01c0a60ec4
kojoluh <dordoedelivery@gmail.com>
2024-02-05 23:11:02 +0000

    added additional maven and related reosurce dependencies to main pub-sub

---
+++ src/main/java/com/javafoster/springbootkafka/SpringbootKafkaPubsubMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.javafoster.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaPubsubMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaPubsubMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javafoster/springbootkafka/SpringbootKafkaPubsubMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafoster.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaPubsubMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
